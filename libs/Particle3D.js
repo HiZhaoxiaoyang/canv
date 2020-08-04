3b08358ca1b3c24a2e64342a1acb8b578c41261f
System Administrator <root@zhaoxiaoyangMacBook-Pro.local>
2020-08-04 19:47:21 +0800

    imitate 3d particle wall; class:Vec3D,Particle3D

--- libs/Particle3D.js
+++ libs/Particle3D.js
@@ -7,22 +7,24 @@ import { Vec3D } from './Vec3D.js'
 const { hypot, tan, atan2, sin, cos, acos, PI } = Math
 
 export class Particle3D {
-    constructor(x, y, z, timeline, ID, depth) {
+    constructor(x, y, z, ctx, ID, depth) {
         this.position = new Vec3D(x, y, z)
-        this.timeline = timeline
-        this.mc = this.attachGraphic(ID, depth)
+        this.ctx = ctx
+        // this.mc = this.attachGraphic(this.position.x, this.position.y, ID, depth)
         this.scale = 100
-        this.render()
+        // this.render()
     }
-    attachGraphic(ID, depth) {
-        return this.timeline.attachMovie(ID, `${ID}_${depth}`, depth)
+    attachGraphic(x, y, ID, depth) {
+        // return this.timeline.attachMovie(ID, `${ID}_${depth}`, depth)
+        return this.ctx.fillRect(x, y, 10, 10)
     }
     render() {
-        const pers = this.position.getPerspective(),
+        const pers = this.position.getPerspective()
         this.screenPos = this.position.persProjectNew(pers)
-        this.mc._x = this.screenPos.x
-        this.mc._y = this.screenPos.y
+        // this.mc._x = this.screenPos.x
+        // this.mc._y = this.screenPos.y
+        this.attachGraphic(this.screenPos.x, this.screenPos.y, 1, 0)
         this._xscale = this._yscale = this.scale * pers
-        swapDepth(100000 - this.position.z * 100)
+        // swapDepth(100000 - this.position.z * 100)
     }
 }
